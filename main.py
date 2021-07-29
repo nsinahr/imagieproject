62da1090ea5d7f589812ddd9b81eb00b45413860
nsinahr <88126369+nsinahr@users.noreply.github.com>
2021-07-28 21:41:13 -0700

    Initial commit bob

---
+++ main.py
@@ -0,0 +1,9 @@
+
+print("Choose one choco or vanilla. Type it out below honey :) ")
+
+if input()== "choco":
+    print("you chose choco! I luv u, your soooo cute")
+
+else:
+    print("who chooses vanilla, youre ugly")
+
